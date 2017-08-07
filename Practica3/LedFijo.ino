9e0109a7677f66de1c9d101cb633e5c5a8da0ae8
Carlos <king_slave_@hotmail.com>
2017-08-07 14:37:38 -0500

    Documentacion de practicas

---
+++ Practica3/LedFijo.ino
@@ -0,0 +1,13 @@
+const int led = 13;
+void setup() {
+  // initialize the digital pin as an output.
+  pinMode(led, OUTPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  digitalWrite(led, HIGH);    // turn the LED off by making the voltage LOW
+  delay(1000);               // wait for a second
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);               // wait for a second
+}
